8d3698e91f378f3193b438b41040f162e340133e
pmatteaz <pmatteaz@gmail.com>
2024-10-29 17:35:10 +0100

    Update apache-hardening-mapped.sh

--- apache-hardening-mapped.sh
+++ apache-hardening-mapped.sh
@@ -678,7 +678,7 @@ configure_ssl() {
 
     cat > "$ssl_conf" << 'EOL'
 # CIS 7.1-7.12: SSL/TLS Configuration
-SSLProtocol all -SSLv3 -TLSv1 -TLSv1.1
+SSLProtocol all -SSLv3
 SSLCipherSuite EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH
 SSLHonorCipherOrder on
 SSLCompression off
