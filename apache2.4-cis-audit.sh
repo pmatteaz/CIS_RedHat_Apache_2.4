f18f07586d3f29509f1aa9488407134eee8f89b2
pmatteaz <pmatteaz@gmail.com>
2024-10-30 11:02:40 +0100

    Update apache2.4-cis-audit.sh

--- apache2.4-cis-audit.sh
+++ apache2.4-cis-audit.sh
@@ -618,11 +618,11 @@ print_section "4 Apache Access Control"
 check_os_root_access() {
     local root_dir_conf=""
     if [ "$DISTRO" = "debian" ]; then
-        root_dir_confs+=$(grep "<Directory /" "$APACHE_PATH/apache2/apache.conf" | awk '/<Directory.*>/{p=1} /<\/Directory>/{p=0} p' )
-        root_dir_confs+=$(grep "<Directory /" "$APACHE_PATH/conf-available/security-access.conf" | awk '/<Directory.*>/{p=1} /<\/Directory>/{p=0} p' )
+        root_dir_confs+=$(awk '/<Directory \/>/{flag=1;next} /<\/Directory>/{flag=0} flag==1' "$APACHE_PATH/apache2/apache.conf" )
+        root_dir_confs+=$(awk '/<Directory \/>/{flag=1;next} /<\/Directory>/{flag=0} flag==1' "$APACHE_PATH/conf-available/security-access.conf" )
     else
-        root_dir_confs+=$(grep "<Directory /" "$APACHE_PATH/conf/httpd.conf" |awk '/<Directory.*>/{p=1} /<\/Directory>/{p=0} p')
-        root_dir_confs+=$(grep "<Directory /" "$APACHE_PATH/conf.d/security-access.conf" |awk '/<Directory.*>/{p=1} /<\/Directory>/{p=0} p')
+        root_dir_confs+=$(awk '/<Directory \/>/{flag=1;next} /<\/Directory>/{flag=0} flag==1' "$APACHE_PATH/conf/httpd.conf" )
+        root_dir_confs+=$(awk '/<Directory \/>/{flag=1;next} /<\/Directory>/{flag=0} flag==1' "$APACHE_PATH/conf.d/security-access.conf" )
     fi
     for root_dir_conf in "${root_dir_confs[@]}"; do
      if [[ "$root_dir_conf" =~ "Require all denied" ]] || [[ "$root_dir_conf" =~ "deny from all" ]]; then
