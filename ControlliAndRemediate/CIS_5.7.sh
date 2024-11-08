ab1242dc5c8eb6e04400807f00b7bade76594fa4
pmatteaz <pmatteaz@gmail.com>
2024-11-08 17:03:53 +0100

    Update CIS_5.7.sh

--- ControlliAndRemediate/CIS_5.7.sh
+++ ControlliAndRemediate/CIS_5.7.sh
@@ -113,7 +113,7 @@ if [ ${#issues_found[@]} -gt 0 ]; then
         print_section "Esecuzione Remediation"
         
         # Backup del file di configurazione
-        timestamp=$(date +%Y%m%d_%H%M%S)
+        timestamp=$(date +%Y%m%d_%H%M%S)_CIS_5.7
         backup_dir="/root/apache_methods_backup_$timestamp"
         mkdir -p "$backup_dir"
         
